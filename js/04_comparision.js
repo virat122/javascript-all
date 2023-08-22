5a10f9a375498efc77e2aceec8b670542fe4e77f
virat122 <121007372+virat122@users.noreply.github.com>
2023-08-22 21:51:25 +0000

    bugg s

---
+++ js/04_comparision.js
@@ -0,0 +1,21 @@
+console.log(2<3);
+console.log(2<3);
+console.log(2<=3);
+console.log(2>=3);
+console.log("3"==3);// true
+console.log(2!=3); //true
+console.log("3"===3)//false
+ console.log("2">0)// true
+
+// paradox
+console.log(null >0) //false
+console.log(null ==0)//false
+console.log(null >=0)//true
+
+console.log(undefined >0) //false
+console.log(undefined ===0)//false
+console.log(undefined ==0)//false
+console.log(undefined >=0)//false
+
+
+
