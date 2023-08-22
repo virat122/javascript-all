3e2dbbf573b1dba21332fc7bbeaad4693dcf2a28
virat122 <121007372+virat122@users.noreply.github.com>
2023-08-22 18:07:09 +0000

    frist lucture

---
+++ js/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 14467;
+let accountEmail="vikrant12@gmail.com"
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState;// undefined 
+
+
+ //accountId=74;// not allowed
+
+ accountEmail="galib@gmail.com"
+ accountPassword="576"
+ accountCity="delhi"
+
+
+ console.log( "data is here");
+ console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
+  /*
+  prefer not to use "var"  becoz of issue in block scope and functional scope
+  */
