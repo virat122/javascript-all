d3b19ff2128930133febd3200b2cadf940416318
virat122 <121007372+virat122@users.noreply.github.com>
2023-08-22 18:40:56 +0000

    untraaced showing

---
+++ js/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treate all js code in newer version
+
+ alert(3+6)// error bcoz we are using nodejs not browser (browser understand document formet )
+
+console.log("dataTypes")
+
+/*
+number => 2 ^ 52
+bigInt 
+string => ""
+boolean => true/f
+null => standalone value
+undefined => 
+symbol =>  used to find uniqueness used in react etc
+object =>
+*/
+
+console.log(typeof "vikrant")
+console.log(typeof null)
+
+console.log(typeof undefined )
+
+console.log(typeof "vikrant")
+
