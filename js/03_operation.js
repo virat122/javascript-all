f294183a47127bfab32fbeb96720f51d68fd27f4
virat122 <121007372+virat122@users.noreply.github.com>
2023-08-22 21:30:41 +0000

    operation  some tricky ques

---
+++ js/03_operation.js
@@ -0,0 +1,35 @@
+console.log("<+===================operation s===============================>")
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+// confusing 
+console.log(2+"2") //22
+console.log("2"+2) //22
+console.log(4/"2")  //2
+console.log("8"/2) //4
+
+console.log(2+2+"4")//44
+
+console.log("2"+2+4)//224
+
+let a="vik"
+let b="hjdf";
+console.log(a/b) //NaN
+
+// console.log(+true) //1
+// console.log(true+ ) //error syntax
+// console.log(true+ 1) //2
+// console.log(+false) //0
+console.log(+"")// 0
+
+
+let p=q=r=s=7;
+console.table([p,q,r,s]) // 7,7,7,7
+
+
+
