e18063913853eb341b256a1e81b62e6a1a30f615
virat122 <121007372+virat122@users.noreply.github.com>
2023-08-24 00:49:36 +0000

    methods string

---
+++ js/06_string.js
@@ -0,0 +1,30 @@
+const name="vikrant "
+const mob=75857878;
+let namee=new String("vikrant ")
+// let p=new String(namee)
+// namee="ppppppppppppppppppp";
+// console.log(typeof p)
+// p="kjdfh";
+// console.log(typeof p)
+// console.log(namee)
+
+
+console.log("my name is "+name+"and mob no is "+mob) //old syntax  "bad to use" 
+
+console.log(`my name is ${ name } and mob no is ${mob}`); // better to use
+
+
+ // string methods 
+ /* .length
+    toLowerCase();
+    .slice()
+    trim()
+    replace()
+    charAt();
+    indexOf()
+    .split()
+    .includes()  return true false
+
+    */
+
+
